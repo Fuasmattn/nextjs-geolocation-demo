a429d2f52fd17de167f5cfd2a2bab12b8b1fcb76
Martin Prinz <martin.prinz@scania.com>
2023-09-14 13:22:24 +0000

    Revert "chore: suppress errors"
    This reverts commit 2ad4a8706781d22732ee7150bc31c7d854b3729a.

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import Map from "ol/Map.js";
 import OSM from "ol/source/OSM.js";
 import TileLayer from "ol/layer/Tile.js";
@@ -6,7 +6,7 @@ import View from "ol/View.js";
 import Feature from "ol/Feature.js";
 import Point from "ol/geom/Point.js";
 import VectorSource from "ol/source/Vector.js";
-import { Vector as VectorLayer } from "ol/layer.js";
+import { Layer, Vector as VectorLayer } from "ol/layer.js";
 import { fromLonLat } from "ol/proj";
 import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style.js";
 import useGeoLocation from "../../hooks/useGeolocation";
@@ -37,77 +37,44 @@ const styles = {
 const DICEMap = ({ vehicles }: any) => {
   const mapRef = useRef<Map>();
   const [geoPosition] = useGeoLocation();
-  const [feature, setFeature] = useState<Feature>();
-  const [featuresLayer, setFeaturesLayer] = useState<VectorLayer<any>>();
-
-  useEffect(
-    () => {
-      if (geoPosition?.coords) {
-        const userPositionMarker = new Feature({
-          type: "geoMarker",
-          geometry: new Point(
-            fromLonLat([
-              geoPosition.coords.longitude,
-              geoPosition.coords.latitude,
-            ])
-          ),
-        });
-
-        setFeature(userPositionMarker);
-
-        const vehiclePositionMarkers = vehicles.map(
-          (vehicle: any) =>
-            new Feature({
-              type: "vehicleMarker",
-              geometry: new Point(
-                fromLonLat([
-                  vehicle.position.longitude,
-                  vehicle.position.latitude,
-                ])
-              ),
-            })
-        );
-
-        const vectorLayer = new VectorLayer({
-          source: new VectorSource({
-            features: [feature, ...vehiclePositionMarkers],
-          }),
-          style: function (feature) {
-            // @ts-ignore
-            return styles[feature.get("type")];
-          },
-        });
-
-        mapRef.current?.addLayer(vectorLayer);
-
-        mapRef.current?.setView(
-          new View({
-            center: fromLonLat([
-              geoPosition.coords.longitude,
-              geoPosition.coords.latitude,
-            ]),
-            zoom: 15,
-          })
-        );
-
-        setFeaturesLayer(vectorLayer);
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [
-      // geoPosition?.coords.latitude,
-      // geoPosition?.coords.longitude,
-      // geoPosition?.coords,
-      // vehicles,
-    ]
-  );
 
   useEffect(() => {
-    if (geoPosition) {
-      const newPosition = new Point(
-        fromLonLat([geoPosition.coords.longitude, geoPosition.coords.latitude])
+    if (geoPosition?.coords) {
+      const userPositionMarker = new Feature({
+        type: "geoMarker",
+        geometry: new Point(
+          fromLonLat([
+            geoPosition.coords.longitude,
+            geoPosition.coords.latitude,
+          ])
+        ),
+      });
+
+      const vehiclePositionMarkers = vehicles.map(
+        (vehicle: any) =>
+          new Feature({
+            type: "vehicleMarker",
+            geometry: new Point(
+              fromLonLat([
+                vehicle.position.longitude,
+                vehicle.position.latitude,
+              ])
+            ),
+          })
       );
-      feature?.setGeometry(newPosition);
+
+      const vectorLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [userPositionMarker, ...vehiclePositionMarkers],
+        }),
+        style: function (feature) {
+          // @ts-ignore
+          return styles[feature.get("type")];
+        },
+      });
+
+      mapRef.current?.addLayer(vectorLayer);
+
       mapRef.current?.setView(
         new View({
           center: fromLonLat([
@@ -117,16 +84,15 @@ const DICEMap = ({ vehicles }: any) => {
           zoom: 15,
         })
       );
-      if (feature) {
-        featuresLayer?.setSource(new VectorSource({ features: [feature] }));
-      }
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    geoPosition,
-    geoPosition?.coords.latitude,
-    geoPosition?.coords.longitude,
-  ]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    mapRef.current?.getAllLayers().forEach((layer: Layer) => {
+      layer.getSource()?.refresh();
+    })
+  }, [geoPosition?.coords])
 
   useEffect(() => {
     if (!mapRef.current) {
